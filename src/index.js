b6e996d8115f1a96977ee5ecac69cf66b4c6bd22
hendrixxD <maticsjnr@gmail.com>
2024-02-10 21:32:26 +0100

    run 127.0.0.1/auth/google to trigger the oauth

--- src/index.js
+++ src/index.js
@@ -15,10 +15,11 @@ const app = express();
 
 let port;
 
+// mongodb default port 27017
 // slightly made a tweek so as not to conflic port numbers
 // Check if Google authentication is enabled
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET && process.env.CALLBACK_URI) {
-  port = process.env.PORT || 27226; //Sets Default to port 3000 if PORT env variable is not set
+  port = process.env.PORT || 27226; //Sets Default to port 27226 if PORT env variable is not set
 } else {
   port = 33456; 
 }
