006340f3ab0c7b31545f0ca7661ae8a7dceaee31
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-02-13 13:58:22 +0100

    added the google profile picture

--- src/Controllers/googleRegistration.js
+++ src/Controllers/googleRegistration.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
-const userModel = require('../Models/userModel')
-const profileModel = require('../Models/profileModel')
-const notificationModel = require('../Models/notificationModel')
-const jwt = require('jsonwebtoken')
-const currentDate = new Date()
-
+require("dotenv").config();
+const userModel = require("../Models/userModel");
+const profileModel = require("../Models/profileModel");
+const notificationModel = require("../Models/notificationModel");
+const jwt = require("jsonwebtoken");
+const currentDate = new Date();
 
 let month = currentDate.getMonth() + 1;
 if (month > 12) {
@@ -16,7 +15,6 @@ if (month < 10) {
 let year = currentDate.getFullYear();
 let date = currentDate.getDate();
 const fullDate = `${date}/${month}/${year}`;
-
 
 function displayTime() {
   const currentDate = new Date();
@@ -33,12 +31,12 @@ function displayTime() {
   return { full_time };
 }
 
-setInterval(function() {
+setInterval(function () {
   const { full_time } = displayTime();
   // console.log(full_time);
-},10000);
+}, 10000);
 
-const fullTime = displayTime().full_time
+const fullTime = displayTime().full_time;
 
 function generateRandomNumber(digits) {
   const min = Math.pow(10, digits - 1);
@@ -47,24 +45,28 @@ function generateRandomNumber(digits) {
 }
 
 function capitalizeFirstLetters(str) {
-  return str.replace(/\b\w/g, function(char) {
+  return str.replace(/\b\w/g, function (char) {
     return char.toUpperCase();
   });
 }
-capitalizeFirstLetters('shallom ekunadyo')
+capitalizeFirstLetters("shallom ekunadyo");
 
 const registerWuthGoogle = async (req, res) => {
   try {
     const reqDets = req.user;
     const findEmail = await userModel.findOne({ email: reqDets._json.email });
-
+    console.log(reqDets);
     // Check if any documents were found with the given email
     if (findEmail) {
-      const payload = {user_id:findEmail._id,name:findEmail.name, role:findEmail.role}
-        const secretKey = process.env.JWT_SECRET
-        const token = jwt.sign(payload, secretKey)
-        await userModel.updateOne({ _id: findEmail._id }, { Status: "Online" });
-        return res.status(200).json(token)
+      const payload = {
+        user_id: findEmail._id,
+        name: findEmail.name,
+        role: findEmail.role,
+      };
+      const secretKey = process.env.JWT_SECRET;
+      const token = jwt.sign(payload, secretKey);
+      await userModel.updateOne({ _id: findEmail._id }, { Status: "Online" });
+      return res.status(200).json(token);
     }
 
     const fullName = capitalizeFirstLetters(reqDets._json.name);
@@ -72,14 +74,15 @@ const registerWuthGoogle = async (req, res) => {
     const lastLetter = fullName.charAt(-1).toUpperCase();
     const randomNumber = generateRandomNumber(4);
     const userId = `A${firstLetter}${lastLetter}${randomNumber}`;
-  
+
     const newUser = new userModel({
       id: userId,
+      avatar: reqDets._json.picture,
       name: fullName,
       email: reqDets._json.email,
-      gender: 'M', 
-      fullTime, 
-      fullDate
+      gender: "M",
+      fullTime,
+      fullDate,
     });
 
     await newUser.save();
@@ -87,31 +90,25 @@ const registerWuthGoogle = async (req, res) => {
     // Assuming profileModel is defined and saving user profile is necessary
     const newProfile = new profileModel({
       id: newUser.id,
-      __id:newUser._id,
+      __id: newUser._id,
+      avatar:newUser.avatar,
       name: newUser.name,
       number: newUser.number,
       email: newUser.email,
       gender: newUser.gender,
-      age: newUser.age // Assuming age is available in the userModel
+      age: newUser.age, // Assuming age is available in the userModel
     });
     await newProfile.save();
 
-    const message = `Hey there, ${newUser.name}! 🌟 Welcome to senexCare! We're thrilled to have you onboard our platform! Get ready for an incredible journey ahead. To make the most of your experience, don't forget to update your details. Let's embark on this exciting adventure together! 🚀`
+    const message = `Hey there, ${newUser.name}! 🌟 Welcome to senexCare! We're thrilled to have you onboard our platform! Get ready for an incredible journey ahead. To make the most of your experience, don't forget to update your details. Let's embark on this exciting adventure together! 🚀`;
     const newNotify = new notificationModel({
-      message
-    })
-    newNotify.save()
-    
+      message,
+    });
+    newNotify.save();
   } catch (error) {
     console.error(error);
-    return res.status(500).json('An error occurred while registering');
+    return res.status(500).json("An error occurred while registering");
   }
 };
 
-
-
-
-
-
-
-module.exports = {registerWuthGoogle}
+module.exports = { registerWuthGoogle };
